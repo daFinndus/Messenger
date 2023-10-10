b4f0923182c401805c32c07670003ea920bb0534
daFinndus <128810466+daFinndus@users.noreply.github.com>
2023-10-10 10:58:16 +0200

    Scaffold instead of Container

--- lib/widgets/chat_page.dart
+++ lib/widgets/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:messenger/widgets/components/chat_box_demo.dart';
+import 'package:messenger/app_colors.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -11,15 +12,27 @@ class ChatPage extends StatefulWidget {
 class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(16.0),
-      child: const Column(
-        children: [
-          ChatBoxDemo(title: "Mary", imagePath: "assets/images/mary.png"),
-          ChatBoxDemo(title: "Leon", imagePath: "assets/images/leon.png"),
-          ChatBoxDemo(title: "Rajesh", imagePath: "assets/images/rajesh.png"),
-          ChatBoxDemo(title: "Thomas", imagePath: "assets/images/thomas.png"),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.primaryColor,
+        title: Text(
+          "Chats",
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: AppColors.brightColor),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.all(16.0),
+          child: const Column(
+            children: [
+              ChatBoxDemo(title: "Mary", imagePath: "assets/images/mary.png"),
+              ChatBoxDemo(title: "Leon", imagePath: "assets/images/leon.png"),
+              ChatBoxDemo(title: "Raj", imagePath: "assets/images/rajesh.png"),
+              ChatBoxDemo(title: "Tom", imagePath: "assets/images/thomas.png"),
+            ],
+          ),
+        ),
       ),
     );
   }
