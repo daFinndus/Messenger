8f092ba8521dd4a87052e81e99b877bb61eb73d6
YangReiskanzler <darrenyeo245@gmail.com>
2023-10-10 19:54:55 +0200

    Changed Settings
    Added Profile,
    All boxes with function: sign out

--- lib/widgets/components/setting_box.dart
+++ lib/widgets/components/setting_box.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:messenger/app_colors.dart';
 
 class CustomSettingBox extends StatefulWidget {
-  final Function function;
   final String title;
-  const CustomSettingBox({super.key, required this.function, required this.title});
+  final Function function;
+
+  const CustomSettingBox(
+      {super.key, required this.title, required this.function});
 
   @override
   State<CustomSettingBox> createState() => _CustomSettingBoxState();
@@ -13,25 +15,24 @@ class CustomSettingBox extends StatefulWidget {
 class _CustomSettingBoxState extends State<CustomSettingBox> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(8.0),
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(4)),
-        child: ElevatedButton(
+    return SizedBox(
+      width: 500.0,
+      height: 60,
+      child: ElevatedButton(
         onPressed: () => widget.function(),
-    style: TextButton.styleFrom(
-    shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(8.0),
-    ),
-    backgroundColor: AppColors.primaryColor,
-    padding: const EdgeInsets.all(8),
-    ),
-    child: Text(
-    widget.title,
-    style: TextStyle(
-    fontSize: 20.0,
-    fontWeight: FontWeight.bold,
-    color: AppColors.brightColor,
-    ),
-    )));
+        style: TextButton.styleFrom(
+          backgroundColor: Colors.white,
+          padding: const EdgeInsets.all(5),
+        ),
+        child: Text(
+          widget.title,
+          style: const TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.teal,
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/setting_page.dart
+++ lib/widgets/setting_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:messenger/app_colors.dart';
 import 'package:messenger/widgets/components/setting_box.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({super.key});
@@ -14,14 +15,39 @@ class _SettingsPageState extends State<SettingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            backgroundColor: AppColors.primaryColor,
+            backgroundColor: AppColors.brightColor,
             title: Text(
               'Settings',
               style: TextStyle(
-                  fontWeight: FontWeight.bold, color: AppColors.brightColor),
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.darkColor),
             )),
-        body: Center(
-            child: Container(
-                margin: const EdgeInsets.all(16.0), child: const Column())));
+      body:  SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              margin: const EdgeInsets.all(8.0),
+              color: Colors.teal,
+              child: const ListTile(
+                title: Text("MokTripleA", style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/Moktriplea.png"),
+                ),
+                  trailing: Icon(Icons.edit, color: Colors.white,)
+              ),
+            ),
+            CustomSettingBox(title: "Themes", function: FirebaseAuth.instance.signOut),
+            CustomSettingBox(title: "Change Password", function: FirebaseAuth.instance.signOut),
+            CustomSettingBox(title: "Language", function: FirebaseAuth.instance.signOut),
+            CustomSettingBox(title: "Sign Out", function: FirebaseAuth.instance.signOut),
+          ],
+        )
+      )
+       );
   }
 }
