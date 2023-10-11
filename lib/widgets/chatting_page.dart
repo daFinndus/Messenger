bf16566650b2c115cadf6a744a78632e51e1a6b4
YangReiskanzler <darrenyeo245@gmail.com>
2023-10-11 23:30:35 +0200

    chatting
    tests

---
+++ lib/widgets/chatting_page.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:messenger/model/message.dart';
+
+class ChattingPage extends ChangeNotifier {
+  //get instance of auth and firestore
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseFirestore_fireStore = FirebaseFirestore.instance;
+
+  //Send Message
+  Future<void> sendMessage(String receiverId, String message) async {
+    // get current user info
+    final String currentUserId = _firebaseAuth.currentUser!.uid;
+    final String currentUserEmail = _firebaseAuth.currentUser!.email.toString();
+    final Timestamp timestamp = Timestamp.now();
+    // create a new message
+    Message newMessage = Message(
+        senderId: CurrentUserId,
+        senderEmail: CurrentUserEmail,
+        receiverId: receiverId,
+        timestamp: timestamp,
+        message: message,
+    );
+
+    // construct chat room id from current user id and receiver id
+
+    //add new message to database
+  }
+//get messages
+}
