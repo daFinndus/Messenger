abb7ebc8148aa34365df84239cb4137ada17de10
daFinndus <128810466+daFinndus@users.noreply.github.com>
2023-11-11 19:26:11 +0100

    Added news page, doesn't work yet

--- lib/widgets/tab_page.dart
+++ lib/widgets/tab_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:messenger/constants/app_colors.dart';
 import 'package:messenger/constants/app_names.dart';
+import 'package:messenger/widgets/top_pages/news_page.dart';
 import 'package:messenger/widgets/top_pages/chat_list_page.dart';
 import 'package:messenger/widgets/top_pages/setting_page.dart';
 
@@ -11,12 +12,13 @@ class TabPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DefaultTabController(
-        length: 2,
+        length: 3,
         child: Scaffold(
           appBar: AppBar(
             backgroundColor: AppColors.primaryColor,
             bottom: const TabBar(
               tabs: [
+                Tab(icon: Icon(Icons.newspaper)),
                 Tab(icon: Icon(Icons.chat)),
                 Tab(icon: Icon(Icons.settings)),
               ],
@@ -28,6 +30,7 @@ class TabPage extends StatelessWidget {
           ),
           body: const TabBarView(
             children: [
+              NewsPage(),
               ChatList(),
               SettingPage(),
             ],
---
+++ lib/widgets/top_pages/news_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:messenger/services/news/news_article.dart';
+import 'package:messenger/services/news/news_api.dart';
+import 'package:messenger/widgets/components/news_tile.dart';
+
+class NewsPage extends StatefulWidget {
+  const NewsPage({super.key});
+
+  @override
+  State<NewsPage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<NewsPage> {
+  NewsAPI client = NewsAPI();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: client.getArticle(),
+      builder:
+          (BuildContext context, AsyncSnapshot<List<NewsArticle>> snapshot) {
+        if (snapshot.hasData) {
+          List<NewsArticle> articles = snapshot.data!;
+          return ListView.builder(
+            itemBuilder: (context, index) => CustomNewsTile(
+              newsArticle: articles[index],
+            ),
+          );
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+}
